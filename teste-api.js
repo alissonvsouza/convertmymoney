e421222579a50ecdea35d1b225fc33c74108e3c5
Alisson Souza <alissonvsouza8@gmail.com>
2019-12-23 16:15:39 -0200

    delete test-api.js file

--- teste-api.js
+++
@@ -1,5 +0,0 @@
-const axios = require('axios')
-
-const url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=%2704-12-2019%27&$top=100&$skip=0&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao'
-
-axios.get(url).then(res => console.log(res.data))
