fb150f6e117680b56e7125ce8853e14cb9b43142
Alisson Souza <alissonvsouza8@gmail.com>
2020-01-04 17:42:39 -0200

    Up to date index.js

--- index.js
+++ index.js
@@ -1,39 +1,7 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+const app = require('./app')()
+const port = process.env.PORT || 3000
 
-const convert = require('./lib/convert')
-const apiBCB = require('./lib/api.bcb')
-
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
-app.use(express.static(path.join(__dirname, 'public')))
-
-app.get('/', async (req, res) => {
-    const cotacao = await apiBCB.getCotacao()
-    res.render('home', {
-        cotacao
-    })
-})
-
-app.get('/cotacao', (req, res) => {
-    const { cotacao, quantidade } = req.query
-    if (cotacao && quantidade) {
-        const conversao = convert.convert(cotacao, quantidade)
-        res.render('cotacao', {
-            error: false,
-            cotacao: convert.toMoney(cotacao),
-            quantidade: convert.toMoney(quantidade),
-            conversao: convert.toMoney(conversao)
-        })
-    } else {
-        res.render('cotacao', {
-            error: 'Valores inválidos'
-        })
-    }
-})
-
-app.listen(3000, err => {
+app.listen(port, err => {
     if (err) {
         console.log('err', err)
     } else {
